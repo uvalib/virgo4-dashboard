85a1fa011f7cd0facfb25db224be5b7aaca0b24f
UVADave <dpg3k@virginia.edu>
2019-11-14 10:25:32 -0500

    Add a 24 hour ingest dashboard

---
+++ jobs/ingest_hours_service.rb
@@ -0,0 +1,116 @@
+SCHEDULER.every '30m', allow_overlapping: false do
+
+  config = CONFIG['INGEST_SIRSI_MARC_CONVERT_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_SIRSI_MARC_CONVERT_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_HATHI_MARC_CONVERT_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_HATHI_MARC_CONVERT_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_TRACKSYS_ENRICH_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+
+  config = CONFIG['INGEST_TRACKSYS_ENRICH_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+
+  config = CONFIG['INGEST_SOLR_UPDATE_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+
+  config = CONFIG['INGEST_SOLR_UPDATE_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_SOLR_DELETE_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+
+  config = CONFIG['INGEST_SOLR_DELETE_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+  config = CONFIG['INGEST_CACHE_QUEUE_IN']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+
+  config = CONFIG['INGEST_CACHE_QUEUE_OUT']
+
+  queue_name = config['queue']
+  metric_name = config['metric']
+  data_sink = "#{config['id']}-hour"
+  last_n_minutes = "#{CONFIG['INGEST_GENERAL']['last_n_hours'].to_i * 60}"
+
+  Processor.aws_queue_metrics( queue_name, metric_name, last_n_minutes, data_sink )
+
+end
