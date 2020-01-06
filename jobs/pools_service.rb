d013ce2664813bde6df932eccac6e0222f28d635
UVADave <dpg3k@virginia.edu>
2020-01-06 10:07:07 -0500

    Add the rare books pool to the dashboard

--- jobs/pools_service.rb
+++ jobs/pools_service.rb
@@ -7,6 +7,13 @@ SCHEDULER.every '60s', allow_overlapping: false do
   data_sink = config['id']
   Processor.http_status_check( service_url, service_title, data_sink )
 
+  config = CONFIG[ 'SOLR_ARCHIVAL_POOL_SERVICE' ]
+
+  service_url = config['url']
+  service_title = config['title']
+  data_sink = config['id']
+  Processor.http_status_check( service_url, service_title, data_sink )
+
   config = CONFIG[ 'SOLR_CATALOG_POOL_SERVICE' ]
 
   service_url = config['url']
@@ -21,14 +28,28 @@ SCHEDULER.every '60s', allow_overlapping: false do
   data_sink = config['id']
   Processor.http_status_check( service_url, service_title, data_sink )
 
-  config = CONFIG[ 'SOLR_SERIALS_POOL_SERVICE' ]
+  config = CONFIG[ 'SOLR_MUSIC_RECORDINGS_POOL_SERVICE' ]
 
   service_url = config['url']
   service_title = config['title']
   data_sink = config['id']
   Processor.http_status_check( service_url, service_title, data_sink )
 
-  config = CONFIG[ 'SOLR_MUSIC_RECORDINGS_POOL_SERVICE' ]
+  config = CONFIG[ 'SOLR_MUSICAL_SCORES_POOL_SERVICE' ]
+
+  service_url = config['url']
+  service_title = config['title']
+  data_sink = config['id']
+  Processor.http_status_check( service_url, service_title, data_sink )
+
+  config = CONFIG[ 'SOLR_RAREBOOKS_POOL_SERVICE' ]
+
+  service_url = config['url']
+  service_title = config['title']
+  data_sink = config['id']
+  Processor.http_status_check( service_url, service_title, data_sink )
+
+  config = CONFIG[ 'SOLR_SERIALS_POOL_SERVICE' ]
 
   service_url = config['url']
   service_title = config['title']
@@ -43,20 +64,6 @@ SCHEDULER.every '60s', allow_overlapping: false do
   Processor.http_status_check( service_url, service_title, data_sink )
 
   config = CONFIG[ 'SOLR_VIDEO_POOL_SERVICE' ]
-
-  service_url = config['url']
-  service_title = config['title']
-  data_sink = config['id']
-  Processor.http_status_check( service_url, service_title, data_sink )
-
-  config = CONFIG[ 'SOLR_MUSICAL_SCORES_POOL_SERVICE' ]
-
-  service_url = config['url']
-  service_title = config['title']
-  data_sink = config['id']
-  Processor.http_status_check( service_url, service_title, data_sink )
-
-  config = CONFIG[ 'SOLR_ARCHIVAL_POOL_SERVICE' ]
 
   service_url = config['url']
   service_title = config['title']
