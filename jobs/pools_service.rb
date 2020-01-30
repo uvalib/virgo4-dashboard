aa1f61ab03f4bdf25e593b1f4fbdb9d9ab8bfe1c
UVADave <dpg3k@virginia.edu>
2020-01-30 08:36:04 -0500

    Add JMRL pool to the pools dashboard

--- jobs/pools_service.rb
+++ jobs/pools_service.rb
@@ -7,6 +7,13 @@ SCHEDULER.every '60s', allow_overlapping: false do
   data_sink = config['id']
   Processor.http_status_check( service_url, service_title, data_sink )
 
+  config = CONFIG[ 'JMRL_POOL_SERVICE' ]
+
+  service_url = config['url']
+  service_title = config['title']
+  data_sink = config['id']
+  Processor.http_status_check( service_url, service_title, data_sink )
+
   config = CONFIG[ 'SOLR_ARCHIVAL_POOL_SERVICE' ]
 
   service_url = config['url']
